0f58d1e9ff143b22a1b8e71f4cd3684c77843362
Muhannadimad <61332284+Muhannadimad@users.noreply.github.com>
2021-04-25 16:45:36 +0300

    muhannad6

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,8 +57,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        //'AuthAdmin' => \App\Http\Middleware\AuthAdmin::class,
-      //  'AuthId' => \App\Http\Middleware\AuthId::class,
+        'AuthAdmin' => \App\Http\Middleware\AuthAdmin::class,
+        'AuthId' => \App\Http\Middleware\AuthId::class,
       //  'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
