8536ca6945a375e325d9a727530e5c4be26c2aa5
ahmadguze <61331921+ahmadguze@users.noreply.github.com>
2021-04-26 15:49:36 +0300

    Update ModelTblController.php

--- app/Http/Controllers/ModelTblController.php
+++ app/Http/Controllers/ModelTblController.php
@@ -289,22 +289,22 @@ class ModelTblController extends Controller
       // configure globally via a JSON object
 
 
-      //  $config = Configuration::instance([
-      //   'cloud' => [
-      //     'cloud_name' => 'hi5',
-      //     'api_key' => '323435588613243',
-      //     'api_secret' => 'cWSgE3yKhL0alVclbqPLsT6PY1g'],
-      //   'url' => [
-      //     'secure' => true]]);
-      //     $cloudinary = new Cloudinary($config);
-      //     $use_has_id = (new UserHasModelController())->user_model_id($id , $request->input('model_id'));
-      //     $use_has_id = $use_has_id->get()['id'];
-      //     foreach ($request->file('images') as $file){
-      //     file::created(['name'=>$file->getClientOriginalName() ,  'user_model_id' => $use_has_id]);
-      //     $cloudinary->uploadApi()->upload((string)$file,
-      //     ["public_id" => pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) , "type" => "private"
-      //      , "resource_type	" => "private" , "folder" => "models/".$id."/predict"."/".$request->input('model_id')]);
-      //     }
+       $config = Configuration::instance([
+        'cloud' => [
+          'cloud_name' => 'hi5',
+          'api_key' => '323435588613243',
+          'api_secret' => 'cWSgE3yKhL0alVclbqPLsT6PY1g'],
+        'url' => [
+          'secure' => true]]);
+          $cloudinary = new Cloudinary($config);
+          $use_has_id = (new UserHasModelController())->user_model_id($id , $request->input('model_id'));
+          $use_has_id = $use_has_id->get()['id'];
+          foreach ($request->file('images') as $file){
+          file::created(['name'=>$file->getClientOriginalName() ,  'user_model_id' => $use_has_id]);
+          $cloudinary->uploadApi()->upload((string)$file,
+          ["public_id" => pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) , "type" => "private"
+           , "resource_type	" => "private" , "folder" => "models/".$id."/predict"."/".$request->input('model_id')]);
+          }
      return "ahmad";
 
 
