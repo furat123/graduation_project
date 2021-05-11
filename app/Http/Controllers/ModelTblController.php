9c1166e80140a83abb4ccc381224bbd3b40e3cc7
ahmadguze <61331921+ahmadguze@users.noreply.github.com>
2021-05-11 18:24:55 +0300

    Update ModelTblController.php

--- app/Http/Controllers/ModelTblController.php
+++ app/Http/Controllers/ModelTblController.php
@@ -439,11 +439,13 @@ class ModelTblController extends Controller
           $resposes[$keyP][$key]=$val;
           
           if($raw['state_id']==1){
-          $res=$cloudinary->adminApi()->asset('models/'.$id.'/predict/'.$request->input('user_id').'/images/'. $resposes[$keyP]['name'] ,
+         try{ $res=$cloudinary->adminApi()->asset('models/'.$id.'/predict/'.$request->input('user_id').'/images/'. $resposes[$keyP]['name'] ,
          ['type' => 'private']);
           foreach( $res as $key => $val)
           $resposes[$keyP][$key]=$val;
-          }
+          }catch(Exception $e){}
+        }
+          
          }
 
          return $resposes;
