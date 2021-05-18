d7fe3951102e44a2150d797bb465ef70bd8f4fa9
ahmadguze <61331921+ahmadguze@users.noreply.github.com>
2021-05-18 19:11:32 +0300

    Update UserHasModelController.php

--- app/Http/Controllers/UserHasModelController.php
+++ app/Http/Controllers/UserHasModelController.php
@@ -34,7 +34,7 @@ class UserHasModelController extends Controller
     {    $uid =  $request->user()->id;
         return response()->json(user_has_model::join('model_tbls' ,'user_has_models.model_id', '=', 'model_tbls.id')
         ->join('users','users.id',"=","user_has_models.user_id")
-         ->where('owner_id',$uid)->where('model_id',$id)->where('accept',0)->select('user_has_models.*','users.name','owner_id')->get(),200); 
+         ->where('owner_id',$uid)->where('user_has_models.model_id',$id)->select('user_has_models.*','users.name','owner_id')->get(),200); 
     }
 
 
