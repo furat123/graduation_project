fdf5dc0242f99e2c3207ae8dcc77a1eed7a89faf
ahmadguze <61331921+ahmadguze@users.noreply.github.com>
2021-05-18 20:00:27 +0300

    Update UserHasModelController.php

--- app/Http/Controllers/UserHasModelController.php
+++ app/Http/Controllers/UserHasModelController.php
@@ -18,9 +18,12 @@ class UserHasModelController extends Controller
       
     }
 
-    public function showforuser( $id )
+    public function showforuser(Request $request, $id )
     {
-        return response()->json(user_has_model::where('user_id',$id)->get(),200);
+        $uid =  $request->user()->id;
+        return response()->json(user_has_model::join('model_tbls' ,'user_has_models.model_id', '=', 'model_tbls.id')
+        ->join('users','users.id',"=","user_has_models.user_id")
+         ->where('owner_id','!=',$uid)->where('user_id',$uid)->where('user_has_models.model_id',$id)->select('user_has_models.*','users.name','owner_id')->get(),200);
       
     }
 
@@ -28,7 +31,7 @@ class UserHasModelController extends Controller
     {
         return response()->json(user_has_model::join('model_tbls' ,'user_has_models.model_id', '=', 'model_tbls.id')
         ->join('users','users.id',"=","user_has_models.user_id")
-         ->where('owner_id',$id)->where('user_id','!=',$id)->select('user_has_models.*','users.name','owner_id')->get(),200); 
+         ->where('owner_id',$id)->where('user_has_models.user_id','!=',$id)->select('user_has_models.*','users.name','owner_id')->get(),200); 
     }
 
     public function showformodel(Request $request ,$id)
